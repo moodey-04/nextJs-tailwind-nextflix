7c31428c6a514c2b1d5b1d97e63cc079e98f5388
Kodey <nuttapon.yumi@gmail.com>
2023-11-12 16:27:57 +0700

    add firebase, auth provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
-    </>
+    </AuthContextProvider>
   );
 }
 
